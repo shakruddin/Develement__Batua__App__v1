b03a515495b389f18eb0924dbeb7a6a5c84c0210
shakruddin <shakruddin16@navgurukul.org>
2018-12-20 16:47:32 +0530

    adding error file and .gitignore

--- js/header.js
+++ js/header.js
@@ -1,3 +1,16 @@
 $('#user-proflie').click(function(){
     $('#user-login').toggle(100);
 })
+
+$('body').scrollTop(300);
+$('.one, .two, .three').click(function() {
+    var sectionTo = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(sectionTo).offset().top - 90
+    }, 1500);
+});
+
+
+$('.try-again').click(function(){
+    window.history.back();
+})
